e504135a8a704aa262e1988112245a53f6d1e46d
Chiam Sheng Zu <Sheng Zu>
2026-03-01 20:51:38 +0800

    spawn rando loc

--- code/spawner.gd
+++ code/spawner.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
 @onready var enemy = load("res://enemy.tscn")
-
+@export var spawn_area = Rect2(Vector2(0,0),Vector2(1152,648))
+	
 func _ready():
 	$Timer.autostart = true
 	$Timer.one_shot = false
@@ -12,4 +13,7 @@ func _ready():
 func _on_timer_timeout():
 	print("spawn")
 	var ene = enemy.instantiate()
+	var x = randf_range(spawn_area.position.x, spawn_area.position.x + spawn_area.size.x)
+	var y = randf_range(spawn_area.position.y, spawn_area.position.y + spawn_area.size.y)
+	ene.global_position = Vector2(x,y)
 	add_child(ene)
