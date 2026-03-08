d53ade911a54a4282c5979c6802c3f0893b99802
Chiam Sheng Zu <Sheng Zu>
2026-03-08 20:14:49 +0800

    bullet + fixed camera

--- code/enemy.gd
+++ code/enemy.gd
@@ -1,12 +1,19 @@
 extends CharacterBody2D
 
+var health = 3
 var SPEED = 50.0
 @onready var player = get_tree().get_first_node_in_group("player")
 
 func _ready():
+	add_to_group("enemy")
 	motion_mode = CharacterBody2D.MOTION_MODE_FLOATING
 
 func _physics_process(_delta):
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction*SPEED
 	move_and_slide()
+
+func take_damage(amount):
+	health -= amount
+	if health <= 0:
+		queue_free()
--- code/player.gd
+++ code/player.gd
@@ -3,6 +3,9 @@ extends CharacterBody2D
 var SPEED = 400.0
 const MAX_HP = 3 
 var HP = MAX_HP
+@export var bullet = load("res://bullet.tscn")
+
+@onready var gun_tip = $GunTip
 
 func _ready():
 	add_to_group("player")
@@ -10,7 +13,21 @@ func _ready():
 	$"Health Bar".max_value = MAX_HP
 	$hurtbox.hurt.connect(_on_hit)
 	motion_mode = CharacterBody2D.MOTION_MODE_FLOATING
+	$Camera2D.enabled = true
 
+func _physics_process(_delta):
+	movement()
+	
+func _input(event):
+	if event.is_action_pressed("shoot"):
+		shoot()
+	
+func shoot():
+	var bullet = bullet.instantiate()
+	get_tree().current_scene.add_child(bullet)
+	bullet.global_position = gun_tip.global_position
+	var dir = (get_global_mouse_position() - gun_tip.global_position).normalized()
+	bullet.direction = dir
 
 func update_health_ui():
 	$"Health Bar/Health Label".text = "HP: %s" % HP
@@ -26,9 +43,6 @@ func _on_hit(damage):
 		
 func _restart_game():
 	get_tree().reload_current_scene()
-
-func _physics_process(_delta):
-	movement()
 
 func movement():
 	var x_mov = Input.get_action_strength("right") - Input.get_action_strength("left")
